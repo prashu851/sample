30b8dc4d734c99d9e85a7d0e9094113e48fe4cca
Sai Prashanth <saiprashanthgunukula@gmail.com>
2020-03-08 15:35:31 +0530

    mobiles API

--- src/mobiles/mobiles.component.ts
+++ src/mobiles/mobiles.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { Mobile } from './mobile';
+import { HttpClient } from '@angular/common/http'
+
 
 @Component({
   selector: 'testing-root-place-2',
   templateUrl: './mobile.component.html'
 })
+@Injectable()
 export class MobileComponent {
   title = 'List of mobiles';
-  phones=[new Mobile('Samsung', 9999),new Mobile('Realme', 10999)]
+  phones=[];
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  getMobiles() {
+    this.httpClient.get("http://localhost:8000/mobiles")
+    .subscribe((resp:[Mobile]) => {
+      this.phones = resp;
+      console.log(resp);
+      
+    })
+  }
 }
+
