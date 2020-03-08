028beef36ffd7bf5c19ed756288c7435ef598be0
Sai Prashanth <saiprashanthgunukula@gmail.com>
2020-03-08 14:50:44 +0530

    API calls to backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ParagraphComponent} from '../packages/paragraph/paragraph.component'
@@ -13,7 +14,8 @@ import { MobileComponent } from 'src/mobiles/mobiles.component';
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent, ParagraphComponent,MobileComponent]
--- src/packages/paragraph/paragraph.component.ts
+++ src/packages/paragraph/paragraph.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { Hero } from './Hero';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'testing-root-place-1',
   templateUrl: './paragraph.component.html'
 })
+@Injectable()
 export class ParagraphComponent {
   title = 'Tour of heroes';
-  heroes=[{'id':1, 'name':'Prashanth'},{'id':2,'name':'Sumanth'}]
-  myFavHero=this.heroes[0].name
+  heroes=[];
+  myFavHero='Not available';
+  abcd="asdfasdfasdfadf"
+  // heroes=[new Hero(1, 'Prashanth'),new Hero(2, 'Sumanth')]
+  // myFavHero=this.heroes[0].name
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  getHeroes() {
+    this.httpClient.get("http://localhost:8000/heroes")
+    .subscribe((resp:[Hero]) => {
+      this.heroes = resp;
+      this.myFavHero = resp[0].name;
+      console.log("coming");
+      console.log(resp);
+      console.log("end");
+    })
+  }
 }
